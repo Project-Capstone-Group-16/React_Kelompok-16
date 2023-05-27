368533f712ee26fe1901bf83943dc5994814f101
AhmadFauzi07 <1911501680@student.budiluhur.ac.id>
2023-05-27 16:23:07 +0700

    fix: layout and space responsive

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import 'antd/dist/reset.css'
+import { ConfigProvider } from 'antd'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: ['Poppins', 'sans-serif'],
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </BrowserRouter>
 )
